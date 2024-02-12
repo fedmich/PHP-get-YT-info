a4bbbf64e7bfccca7769883ca7c3bc474c111807
Federico <fedmich@gmail.com>
2024-02-12 17:27:14 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,9 +2,12 @@
 // Get video_id from $_GET['id']
 $vid_id = isset($_GET['id']) ? $_GET['id'] : '';
 
-// If vid_id is empty, print '.'
+// Initialize response array
+$response = array();
+
+// If vid_id is empty, set response and return
 if (empty($vid_id)) {
-    echo '.';
+    $response['result'] = '.';
 } else {
     // Construct YouTube video URL
     $url_video = "https://www.youtube.com/v/{$vid_id}?feature=youtube_gdata_player";
@@ -24,22 +27,25 @@ if (empty($vid_id)) {
     $page_content = curl_exec($ch);
     if (curl_errno($ch)) {
         // Handle cURL error
-        echo 'Curl error: ' . curl_error($ch);
+        $response['error'] = 'Curl error: ' . curl_error($ch);
     } else {
         // Use regex to extract video_id and title
         preg_match($regex_pattern, $page_content, $matches);
 
-        // Create an associative array with vid_id and title
-        $ret = array(
+        // Set response with vid_id and title as JSON object
+        $response['result'] = array(
             'vid_id' => $vid_id,
             'title' => !empty($matches[1]) ? strip_tags($matches[1]) : '', // Strip HTML tags from the title if matches[1] is not empty
         );
-
-        // Print JSON-encoded result
-        echo json_encode($ret);
     }
 
     // Close cURL resource
     curl_close($ch);
 }
+
+// Set JSON content type header
+header('Content-Type: application/json');
+
+// Print JSON-encoded response
+echo json_encode($response);
 ?>
